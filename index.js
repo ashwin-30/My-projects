761844e78121cabcaff68a28acca0110c637aaa0
ashwin-30 <ashwin3082003@gmail.com>
2024-01-05 16:41:35 +0530

    created my project repository and html,css,js

---
+++ index.js
@@ -0,0 +1,40 @@
+// go-live
+function goLiveDice() {
+    window.open("https://ashwin-30.github.io/dice-game/", "_blank");
+}
+
+function goLiveSimon() {
+    window.open("https://ashwin-30.github.io/simon-game/", "_blank");
+}
+
+function goLiveDrum() {
+    window.open("https://ashwin-30.github.io/drum-kit/", "_blank");
+}
+
+function goLiveTin() {
+    window.open("https://ashwin-30.github.io/tindog/", "_blank");
+}
+
+function goLiveColor() {
+    window.open("https://ashwin-30.github.io/drum-kit/", "_blank");
+}
+// view code 
+function viewDice() {
+    window.open("https://github.com/ashwin-30/dice-game", "_blank");
+}
+
+function viewSimon() {
+    window.open("https://github.com/ashwin-30/simon-game", "_blank");
+}
+
+function viewDrum() {
+    window.open("https://github.com/ashwin-30/drum-kit", "_blank");
+}
+
+function viewTin() {
+    window.open("https://github.com/ashwin-30/tindog", "_blank");
+}
+
+function viewColor() {
+    window.open("https://github.com/ashwin-30/colour-flipper", "_blank");
+}
